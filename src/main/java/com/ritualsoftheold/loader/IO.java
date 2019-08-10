17129e2fb06bc689cf8b098bb29efb94dc870187
Pawlost <mr.p.balusek@gmail.com>
2019-08-10 14:06:05 +0200

    Loading assets

--- src/main/java/com/ritualsoftheold/loader/IO.java
+++ src/main/java/com/ritualsoftheold/loader/IO.java
@@ -1,9 +1,14 @@
 package com.ritualsoftheold.loader;
 
 import com.jme3.asset.AssetManager;
+import com.jme3.asset.plugins.FileLocator;
 import com.jme3.scene.Spatial;
+import com.jme3.scene.plugins.gltf.GltfModelKey;
+import com.jme3.texture.Texture;
+import com.jme3.texture.Texture2D;
 
-import java.io.IOException;
+import java.io.File;
+import java.util.Objects;
 
 public class IO {
     public AssetManager assetManager;
@@ -11,7 +16,16 @@ public class IO {
         this.assetManager = assetManager;
     }
 
-    public Spatial loadAsset(String file){
-        return assetManager.loadModel(file);
+    public Spatial loadAsset(String asset) {
+        File file = new File(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResource("models/"+asset)).getFile());
+        assetManager.registerLocator(file.getParent(), FileLocator.class);
+        GltfModelKey key = new GltfModelKey(asset);
+        return assetManager.loadModel(key);
+    }
+
+    public Texture loadTexture(String texture){
+        File file = new File(Objects.requireNonNull(ClassLoader.getSystemClassLoader().getResource("textures/"+texture)).getFile());
+        assetManager.registerLocator(file.getParent(), FileLocator.class);
+        return assetManager.loadTexture(texture);
     }
 }
--- src/main/java/com/ritualsoftheold/loader/ModelLoader3D.java
+++ src/main/java/com/ritualsoftheold/loader/ModelLoader3D.java
@@ -1,23 +1,33 @@
 package com.ritualsoftheold.loader;
 
 import com.jme3.asset.AssetManager;
-import com.jme3.scene.Geometry;
+import com.jme3.material.Material;
 import com.jme3.scene.Spatial;
-import com.jme3.system.JmeSystem;
-import com.jme3.system.JmeSystemDelegate;
-
-import java.net.URL;
+import com.jme3.texture.Texture;
 
 public class ModelLoader3D {
 
-    private static final String FORMAT = ".gltf";
+    private static final String MODEL_FORMAT = ".gltf";
+    private static final String TEXTURE_FORMAT = ".png";
+    private static final String TEXTURE_MARK =  "-texture-";
     private IO input;
+    private Material material;
 
     public ModelLoader3D(AssetManager assetManager){
+        material = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
         input = new IO(assetManager);
     }
 
-    public Spatial getAsset(String asset){
-        return input.loadAsset(asset+"."+FORMAT);
+    public Spatial getAsset(String assetName, int id){
+        Spatial asset = input.loadAsset(assetName + MODEL_FORMAT);
+        Texture texture = input.loadTexture(assetName+TEXTURE_MARK+id+TEXTURE_FORMAT);
+        material.setTexture("ColorMap", texture);
+        asset.setMaterial(material);
+        asset.updateGeometricState();
+        return asset;
+    }
+
+    public Material getMaterial() {
+        return material;
     }
 }
