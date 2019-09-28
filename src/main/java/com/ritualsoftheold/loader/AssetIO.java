23652bbdb8991616d8d2a10b935375f059076510
Pawlost <mr.p.balusek@gmail.com>
2019-09-28 13:30:22 +0200

    Updated TerraCore to new commit

--- src/main/java/com/ritualsoftheold/loader/AssetIO.java
+++ src/main/java/com/ritualsoftheold/loader/AssetIO.java
@@ -67,7 +67,6 @@ public class AssetIO {
             }
         }
 
-
         Image newImage = image.getScaledInstance(width, height, Image.SCALE_DEFAULT);
 
         Graphics2D graphics2D = bufferedImage.createGraphics();
